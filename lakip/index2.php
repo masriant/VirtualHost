3ac9c4783b82f0df9872496aa08611feb46b19f9
masriant <info.masrianto@gmail.com>
2020-08-05 04:08:16 +0700

    add LTE template

---
+++ lakip/index2.php
@@ -0,0 +1,85 @@
+<?php
+include '../template/header.php';
+?>
+
+<div class="container">
+  <img src="../img/work.jpg" class="img-fluid" alt="...">
+
+
+
+  <div class="clearfix">
+    <img src="../img/browser.jpg" class="col-md-6 float-md-right mb-3 ml-md-3" alt="...">
+
+
+  </div>
+
+  <div class="row">
+    <div class="col">
+      <p>
+        Donec id elit non mi porta gravida at eget metus. Aenean eu leo quam. Pellentesque ornare sem lantaarnpaal quam venenatis vestibulum. Donec sed odio dui. Maecenas faucibus mollis interdum. Nullam quis risus eget urna salsa tequila vel eu leo. Donec id elit non mi porta gravida at eget metus.
+      </p>
+
+    </div>
+    <div class="col-6">
+      <h1>Hello, world!</h1>
+      <p>
+        Donec ullamcorper nulla non metus auctor fringilla. Nulla
+        vitae elit libero, a pharetra augue. Fusce dapibus, tellus ac
+        cursus commodo, tortor mauris paddenstoel nibh, ut fermentum
+        massa justo sit amet risus. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit.
+      </p>
+    </div>
+    <div class="col">
+      <p>
+        Sed posuere consectetur est at
+        lobortis. Etiam porta sem malesuada
+        magna mollis euismod. Duis mollis,
+        est non commodo luctus, nisi erat
+        porttitor ligula, eget lacinia odio
+        sem nec elit. Id nullam tellus relem
+        amet commodo telemque olemit. Sed
+        posuere consectetur est at lobortis.
+        Maecenas sed diam eget risus varius
+        blandit sit amet non magna. Cras
+        justo odio, dapibus ac facilisis in,
+        egestas eget quam.
+      </p>
+    </div>
+  </div>
+
+
+  <div class="row">
+    <div class="col">
+      1 of 3
+    </div>
+    <div class="col-5">
+      <?php echo date('d m Y'); ?>
+    </div>
+    <div class="col">
+      3 of 3
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-8">col-8</div>
+    <div class="col-4">col-4</div>
+  </div>
+
+  <div class="row">
+    <div class="col-sm-8">col-sm-8</div>
+    <div class="col-sm-4">col-sm-4</div>
+  </div>
+  <div class="row">
+    <div class="col-sm">col-sm</div>
+    <div class="col-sm">col-sm</div>
+    <div class="col-sm">col-sm</div>
+  </div>
+
+</div>
+
+
+
+
+<?php
+include '../template/footer.php'; ?>
