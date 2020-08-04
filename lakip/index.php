4751e38cbd53a44a708fa07ed8d41b5b3244daf9
masriant <info.masrianto@gmail.com>
2020-08-04 11:15:14 +0700

    adminLTE

--- lakip/index.php
+++ lakip/index.php
@@ -1,25 +1,85 @@
 <?php 
-echo date('d m Y');
-
+include '../template/header.php';
 ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <div class="container">
+    <img src="../img/work.jpg" class="img-fluid" alt="...">
+    
+    
+    
+    <div class="clearfix">
+  <img src="../img/browser.jpg" class="col-md-6 float-md-right mb-3 ml-md-3" alt="...">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
+</div>
+
+  <div class="row">
+    <div class="col">
+    <p>
+    Donec id elit non mi porta gravida at eget metus. Aenean eu leo quam. Pellentesque ornare sem lantaarnpaal quam venenatis vestibulum. Donec sed odio dui. Maecenas faucibus mollis interdum. Nullam quis risus eget urna salsa tequila vel eu leo. Donec id elit non mi porta gravida at eget metus.
+  </p>
+
+    </div>
+    <div class="col-6">
     <h1>Hello, world!</h1>
+    <p>
+  Donec ullamcorper nulla non metus auctor fringilla. Nulla 
+vitae elit libero, a pharetra augue. Fusce dapibus, tellus ac 
+cursus commodo, tortor mauris paddenstoel nibh, ut fermentum 
+massa justo sit amet risus. Lorem ipsum dolor sit amet, 
+consectetur adipiscing elit.
+</p>
+    </div>
+    <div class="col">
+    <p>
+   Sed posuere consectetur est at 
+lobortis. Etiam porta sem malesuada 
+magna mollis euismod. Duis mollis, 
+est non commodo luctus, nisi erat 
+porttitor ligula, eget lacinia odio 
+sem nec elit. Id nullam tellus relem 
+amet commodo telemque olemit. Sed 
+posuere consectetur est at lobortis. 
+Maecenas sed diam eget risus varius 
+blandit sit amet non magna. Cras 
+justo odio, dapibus ac facilisis in, 
+egestas eget quam.
+ </p>
+    </div>
+  </div>
 
-    <!-- Optional JavaScript -->
-    <!-- Popper.js first, then Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
-  </body>
-</html>
+
+  <div class="row">
+    <div class="col">
+      1 of 3
+    </div>
+    <div class="col-5">
+      <?php echo date('d m Y'); ?>
+    </div>
+    <div class="col">
+      3 of 3
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-8">col-8</div>
+    <div class="col-4">col-4</div>
+  </div>
+
+  <div class="row">
+    <div class="col-sm-8">col-sm-8</div>
+    <div class="col-sm-4">col-sm-4</div>
+  </div>
+  <div class="row">
+    <div class="col-sm">col-sm</div>
+    <div class="col-sm">col-sm</div>
+    <div class="col-sm">col-sm</div>
+  </div>
+
+</div>
+
+
+
+
+<?php 
+include '../template/footer.php'; ?>
