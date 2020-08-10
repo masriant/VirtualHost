004abf0826a0058a1f1bbf63e612942fc6a07cbe
masriant <info.masrianto@gmail.com>
2020-08-10 15:49:51 +0700

    datatable

---
+++ DataTables/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="DataTables/datatables.min.css"/>
+ 
+<script type="text/javascript" src="DataTables/datatables.min.js"></script>
+	<title>Table</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ DataTables/index1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.21/b-1.6.3/b-flash-1.6.3/b-html5-1.6.3/b-print-1.6.3/kt-2.5.2/rg-1.1.2/sp-1.1.1/sl-1.3.1/datatables.min.css"/>
+ 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.21/b-1.6.3/b-flash-1.6.3/b-html5-1.6.3/b-print-1.6.3/kt-2.5.2/rg-1.1.2/sp-1.1.1/sl-1.3.1/datatables.min.js"></script>
+	<title>Table</title>
+</head>
+<body>
+
+</body>
+</html>
